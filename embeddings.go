eaee2e216735f4ae78dfdd9d0b347affcd131cde
David Fritz <f@unitstep.org>
2026-03-02 20:04:25 -0700

    add license headers

--- embeddings.go
+++ embeddings.go
@@ -1,3 +1,7 @@
+// Copyright 2025 David Fritz. All rights reserved.
+// This software may be modified and distributed under the terms of the BSD
+// 2-clause license. See the LICENSE file for details.
+
 package vdb
 
 import (
